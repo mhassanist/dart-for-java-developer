b9e27e4656ab869e8d8840a28041e7c823f53695
mhassanist <m.hassan@ogr.iu.edu.tr>
2019-06-29 10:09:47 +0300

    Essentials of Dart (Before OOP)
    Essentials of Dart (variables, conditionals, loops, functions)

--- essentials.dart
+++ essentials.dart
@@ -1,8 +1,6 @@
-//The esentials
+//Quick Guide to the esentials of dat language referring to Java development// 
 
-//main function as the starting point of an application.
-/* Note: There is no need to be embeeded in class like Java. You can write OOP and Functional programming in Dart (kinda like C++)*/
-
+//main function as the starting point of an application.There is no need to be embeeded in class like Java. You can write OOP and Functional programming in Dart (kinda like C++)
 main() {
   //Print string. String can be single or double qoutes (like PHP)
   print("Hello World");
@@ -35,4 +33,57 @@ main() {
   print(dNUmber); // prints > 5.12
 
   print(s);
+
+  //Conditionals and loops are the same as in Java
+  // foreach also is there with slight difference
+  List names = ["Mohammed", "Samy", "John", "Peter"];
+  for (String name in names) {
+    print(name);
+  }
+
+  //Functions
+  //Functions also are nearly the same as in Java.
+  print(add(5, 8));
+
+  //It supports also lamda functions in the form:
+  //[return_type]function_name(parameters)=>expression;
+  print(add2(23, 89));
+  print(add3(51, 82));
+
+  //And it has optional paramters
+  print(optoinalAdd(12, 12));
+  print(optoinalAdd(12, 13));
+  print(optoinalAdd(12, 13, 14, 15));
+
+  //And default parameters
+  print(optoinalDefaultAdd(1, 2));
+  print(optoinalDefaultAdd(1, 2, c: 3));
+  print(optoinalDefaultAdd(1, 2, c: 3, d: 4));
+
+  //Exceptional Handling
+  try {
+    double result = 12 / 4;
+    print("The result is $result");
+  } catch (e, s) {
+    print("The exception thrown is $e");
+    print("STACK TRACE \n $s");
+  }
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int add2(int a, int b) => a + b;
+int add3(a, b) => a + b; //as type can be inferred
+
+int optoinalAdd(int a, int b, [int c, int d]) {
+  if (c != null && d != null)
+    return a + b + c + d;
+  else
+    return a + b;
+}
+
+int optoinalDefaultAdd(int a, int b, {c = 0, d = 0}) {
+  return a + b + c + d;
 }
